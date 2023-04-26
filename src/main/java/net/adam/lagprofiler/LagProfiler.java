8754324853efd5d611782a04224546f44f46f35b
Adam Ebid <78551691+AdamEbid1@users.noreply.github.com>
2023-04-25 20:33:54 -0400

    Update LagProfiler.java

--- src/main/java/net/adam/lagprofiler/LagProfiler.java
+++ src/main/java/net/adam/lagprofiler/LagProfiler.java
@@ -1,49 +1,8 @@
-//package net.adam.lagprofiler;
-//
-//import com.mojang.logging.LogUtils;
-//import net.minecraftforge.api.distmarker.Dist;
-//import net.minecraftforge.common.MinecraftForge;
-//import net.minecraftforge.eventbus.api.IEventBus;
-//import net.minecraftforge.eventbus.api.SubscribeEvent;
-//import net.minecraftforge.fml.common.Mod;
-//import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-//import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-//import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-//import org.slf4j.Logger;
-//
-//// The value here should match an entry in the META-INF/mods.toml file
-//@Mod(LagProfiler.MOD_ID)
-//public class LagProfiler {
-//    public static final String MOD_ID = "lagprofiler";
-//    private static final Logger LOGGER = LogUtils.getLogger();
-//    public LagProfiler() {
-//        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-//
-//        modEventBus.addListener(this::commonSetup);
-//
-//        MinecraftForge.EVENT_BUS.register(this);
-//    }
-//
-//    private void commonSetup(final FMLCommonSetupEvent event) {
-//    }
-//    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-//    public static class ClientModEvents {
-//        @SubscribeEvent
-//        public static void onClientSetup(FMLClientSetupEvent event) {
-//        }
-//    }
-//}
-
 package net.adam.lagprofiler;
 
 import net.adam.lagprofiler.Profiler.Profiler;
 import com.mojang.logging.LogUtils;
-//import net.minecraft.core.BlockPos;
-//import net.minecraft.world.entity.Entity;
-//import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
-//import net.minecraft.world.level.block.entity.BlockEntity;
-//import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
@@ -57,9 +16,6 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.common.util.LogicalSidedProvider;
 import net.minecraftforge.server.ServerLifecycleHooks;
-
-
-//import net.minecraftforge.fml.server.ServerLifecycleHooks;
 
 import org.slf4j.Logger;
 
